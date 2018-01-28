1bfddc5e530c8dbb37a59c943a2ec63fb1e6546a
Amal Sudama <cds.sudama@gmail.com>
2018-01-27 19:36:33 -0500

    add config for host amal

---
+++ host-amal/zsh/configs/post/status.im.zsh
@@ -0,0 +1,2 @@
+VERSION=`npm -v status-dev-cli`
+echo "Remember to keep updated with status-dev-cli. Current version: ${VERSION}"
---
+++ zsh/configs/post/javas.zsh
@@ -0,0 +1,2 @@
+alias java7='export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home'
+alias java8='export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home'
